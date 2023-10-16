da83cbb134578aaacce06c4c2c6fa825d86195e9
xhuang35 <xhuang35@buffalo.edu>
2023-10-16 17:43:43 -0400

    first commit

---
+++ src/main/java/com/jeff/JobListing/JobListingApplication.java
@@ -0,0 +1,26 @@
+package com.jeff.JobListing;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+
+public class JobListingApplication {
+
+	@Bean
+    public GroupedOpenApi controllerApi() {
+        return GroupedOpenApi.builder()
+                .group("controller-api")
+                .packagesToScan("com.jeff.JobListingr") // Specify the package to scan
+                .build();
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobListingApplication.class, args);
+	}
+
+}
