2ad84c1a9be7a8b89c8df7ee4113ff3b5cc024f0
xhuang35 <xhuang35@buffalo.edu>
2024-01-23 00:04:58 -0500

    CrossOrigin

--- src/main/java/com/jeff/JobListing/Controller/PostController.java
+++ src/main/java/com/jeff/JobListing/Controller/PostController.java
@@ -13,6 +13,7 @@ import com.jeff.JobListing.Repository.SearchPostRepository;
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,7 @@ public class PostController {
         return "Welcome to spring boot heroku demo";
     }
 
+    @CrossOrigin(origins = "https://job-hunting-app-cb9818296d91.herokuapp.com")
     @GetMapping(value="/posts")
     public  List<Post> getAllPosts() {
         return repo.findAll();
